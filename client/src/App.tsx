5bb099160951273fec502e83dc8f92682526772b
Yaroslav Pidlubnyi <ybiuza3@gmail.com>
2021-12-01 20:12:06 +0100

    Updated App.tsx text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,14 @@
 export const App = () => (
     <div className="App">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
-        expedita molestias a facilis rerum incidunt minus voluptas cupiditate,
-        dolore, beatae, cumque doloribus nostrum hic soluta nihil cum quibusdam
-        placeat quis! Ipsam similique, quas explicabo tempora pariatur dolore
-        deserunt enim recusandae cumque rem beatae repellat, possimus quaerat
-        cupiditate officiis itaque sequi earum? Minus iusto perferendis rem
-        deserunt fuga quidem porro consequatur. Animi, provident. Eveniet
-        officia labore veritatis repellat possimus velit, natus nihil ducimus,
-        incidunt cupiditate illo repellendus neque provident magni nobis
-        accusamus, odit qui? Nulla laboriosam sit ipsum sunt libero sapiente?
-        Ex, tenetur? Natus assumenda consequuntur omnis, modi, quod, culpa
-        debitis incidunt et at cum laudantium repellat provident totam. At
-        reprehenderit magnam consectetur fugiat officiis laudantium dignissimos
-        earum, necessitatibus voluptas hic. Commodi blanditiis quos error vitae
-        totam explicabo omnis maxime rerum ab necessitatibus fuga distinctio,
-        veniam corporis nisi eius voluptates aliquam iste optio laborum libero!
-        Nesciunt voluptatibus unde optio molestiae modi!
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ducimus eaque incidunt iure, magnam
+            similique sunt ut? Amet autem dignissimos dolorum eum laboriosam molestias nam quos, repellat saepe
+            sint voluptatem!
+        </div>
+        <div>
+            Adipisci nostrum quis sit ullam. Autem commodi inventore possimus quia rem similique tempora, totam?
+            Accusamus accusantium at delectus, deleniti dolores, eaque id ipsam molestiae nam provident quidem
+            vitae voluptatem, voluptates!
+        </div>
     </div>
 );
