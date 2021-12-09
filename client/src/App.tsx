c60e991bbc2e40858cd8ee1df259b12fb5270eaa
Jan Moták <61924776+motakjan@users.noreply.github.com>
2021-12-09 21:10:51 +0100

    created basic layout (#6)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,45 @@
-import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { Authentication } from './pages/Authentication';
-import { useColorMode } from './context/ColorModeContext';
 
-export const App: React.FC = () => {
-    const { mode,toggleColorMode } = useColorMode();
-
-    return  (
-        <Box
-            sx={{
-                width: '100%',
-            }}
-        >
-            {mode} mode
-            <IconButton
-                sx={{
-                    ml: 1 
-                }}
-                onClick={toggleColorMode}
-                color="inherit"
-            >
-                {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-            <Authentication />
-        </Box>
-    );
-}
+export const App: React.FC = () => (
+    <Box
+        sx={{
+            width: '100%',
+        }}
+    >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt harum
+        distinctio aspernatur, repudiandae sint molestias architecto beatae ut,
+        magnam eligendi voluptate sapiente tenetur mollitia quasi. Laudantium
+        architecto quidem inventore itaque. Minima, est voluptates? Consequatur
+        neque sit, officia magni sed ut laborum fugit dolorum ipsum architecto
+        animi aut nemo dolorem cum sequi voluptatem perferendis earum quaerat
+        laboriosam corrupti voluptates velit beatae. Necessitatibus autem
+        incidunt adipisci odio inventore deserunt doloremque eos soluta,
+        distinctio, illo cupiditate molestias recusandae neque nobis, maxime
+        pariatur commodi eveniet nesciunt. Sint eligendi quisquam asperiores
+        numquam accusantium eaque. Reiciendis. Quo aut pariatur voluptatem
+        impedit veniam quasi quidem placeat molestiae repudiandae hic, quae sint
+        explicabo error fugit ex fugiat ad. Quibusdam et eum officia ipsa
+        corporis deleniti ipsum fugit ex? Doloribus, aliquid? Pariatur
+        cupiditate commodi harum perferendis, error facilis totam facere quidem!
+        Ratione excepturi quos molestiae libero dolores veniam mollitia
+        repudiandae nostrum reiciendis vero, magnam iure rem harum provident
+        non! Sunt consectetur fugiat excepturi, autem laboriosam saepe
+        aspernatur, ut iure fugit doloribus earum esse rem, vitae itaque cumque
+        sint. Id in vitae rerum, iusto deserunt alias. Molestias rerum est
+        placeat. Quidem iusto et voluptas dolor similique porro vel enim minus,
+        omnis iste nobis illum voluptatum, nihil corrupti id. Neque, quidem
+        labore. Rerum non optio laboriosam, sed praesentium voluptatum ipsa
+        dignissimos! Quia voluptates quae ipsum repellat quo. Porro officia non
+        perspiciatis nam velit deleniti quae asperiores temporibus iste
+        voluptatibus, facilis minima nostrum vitae? Pariatur eveniet magni
+        dolorum itaque vero consequatur quae. Expedita, quaerat adipisci
+        sapiente magni est cumque commodi nihil, ab numquam officiis non quasi
+        veniam quibusdam error. Fugiat voluptas dolor magnam optio aspernatur
+        ipsum. Ad fugiat debitis eligendi consequuntur dolores. Sint unde maxime
+        perferendis, nobis magni, ipsam libero ex quae dignissimos commodi ut ab
+        velit suscipit illum cumque pariatur! Necessitatibus delectus maiores ut
+        numquam. Error cupiditate debitis quam cumque voluptatibus.
+        <Authentication />
+    </Box>
+);
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,19 +1,166 @@
 import { ThemeProvider } from '@emotion/react';
-import { createTheme, CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, Tooltip } from '@mui/material';
 import { getCurrentTheme } from '../../assets/theme';
 import { useColorMode } from '../../context/ColorModeContext';
-import * as React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import NoteAddRoundedIcon from '@mui/icons-material/NoteAddRounded';
+import AssignmentReturnedIcon from '@mui/icons-material/AssignmentReturned';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { DrawerHeader, Drawer, AppBar } from './LayoutStyledComponents';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import React from 'react';
 
+const tasksIcons = [<EventNoteIcon />, <NoteAddRoundedIcon />, <AssignmentReturnedIcon /> ];
+const profileIcons = [<DateRangeIcon />, <MailIcon />, <PersonIcon />, <SettingsIcon />];
 
 export const Layout: React.FC<React.ReactNode> = ({ children }) => {
-    const { mode } = useColorMode();
-    const theme = React.useMemo(() => createTheme(getCurrentTheme(mode)), [mode]);
+    const { mode, toggleColorMode } = useColorMode();
+
+    const theme = React.useMemo(
+        () => createTheme(getCurrentTheme(mode)),
+        [mode]
+    );
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            {children}
+            <Box
+                sx={{
+                    display: 'flex' 
+                }}
+            >
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    open={open}
+                >
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            sx={{
+                                marginRight: '36px',
+                                ...open && {
+                                    display: 'none' 
+                                },
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                        >
+                            NotesApp
+                        </Typography>
+                        <Tooltip 
+                            title={'Switch to ' + (mode === 'light' ? 'dark' : 'light') + ' theme'} 
+                            placement="bottom-start"
+                        >
+                            <IconButton
+                                sx={{
+                                    ml: 'auto' 
+                                }}
+                                onClick={toggleColorMode}
+                                color="inherit"
+                            >
+                                {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                            </IconButton>
+                        </Tooltip>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    open={open}
+                >
+                    <DrawerHeader>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? 
+                                <ChevronRightIcon />
+                                : 
+                                <ChevronLeftIcon />
+                            }
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    <List>
+                        {['Tasks', 'Add Task', 'Join Task'].map(
+                            (text, index) => (
+                                <Tooltip 
+                                    title={text} 
+                                    placement="right-start"
+                                >
+                                    <ListItem
+                                        button
+                                        key={text}
+                                    >
+                                        <ListItemIcon >                          
+                                            { tasksIcons[index] }
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItem>
+                                </Tooltip>
+                            )
+                        )}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['Inbox', 'Calendar', 'Profile', 'Settings'].map((text, index) => (
+                            <Tooltip 
+                                title={text} 
+                                placement="right-start"
+                            >
+                                <ListItem
+                                    button
+                                    key={text}
+                                >
+                                    <ListItemIcon>
+                                        {profileIcons[index]}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            </Tooltip>
+                        ))}
+                    </List>
+                </Drawer>
+                <Box
+                    component="main"
+                    sx={{
+                        flexGrow: 1, p: 3 
+                    }}
+                >
+                    <DrawerHeader />
+                    {children}
+                </Box>
+            </Box>
         </ThemeProvider>
-    )
-}
-
+    );
+};
