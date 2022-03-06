4fedfa399c13c53c2af6cd1c71ce6d6b424e6938
Yaroslav Pidlubnyi <ybiuza3@gmail.com>
2022-03-06 15:49:22 +0100

    Completed available routes yup validation, changed IconButton to Icon FE

--- server/src/middlewares/validate.ts
+++ server/src/middlewares/validate.ts
@@ -2,11 +2,14 @@ import {NextFunction, Request, Response} from 'express';
 
 export const validate = (schema:any) => async (req:Request, res:Response, next:NextFunction) => {
     // TODO validate query and params
-    const body = req.body;
     try {
-        await schema.validate(body,{
+        await schema.validate({
+            body: req.body,
+            query: req.query,
+            params: req.params,
+        },{
             abortEarly: false
-        });
+        })
         next();
     } catch(error) {
         return res.status(400).json({error})
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import * as AuthController from '../controllers/auth.controller';
 import express from 'express';
 import {validate} from "../middlewares/validate";
-import {loginSchema} from "../validators/auth.schemas";
+import {loginSchema, registerSchema} from "../validators/auth.schemas";
 
 const authRouter = express.Router();
 
-authRouter.post('/register', AuthController.registerHandler);
+authRouter.post('/register', validate(registerSchema), AuthController.registerHandler);
 authRouter.post('/login', validate(loginSchema), AuthController.loginHandler);
 authRouter.get('/isLoggedIn', AuthController.isLoggedInHandler);
 
