7a72ad0251597e7e70bcc6b4cd1a9a255c370f2e
Michal Ščepka <scepkamichal1@gmail.com>
2022-03-06 21:59:03 +0100

    Add Snackbar with error message

--- client/src/pages/Authentication/Authentication.tsx
+++ client/src/pages/Authentication/Authentication.tsx
@@ -2,25 +2,30 @@ import { authApi } from '../../api/auth';
 import { AxiosError } from 'axios';
 import { GetAuthResType, LoginDataType } from '../../types/authTypes';
 import { useMutation } from 'react-query';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Login } from '../../components/Authentication/Login';
 import { GradientBox } from '../../components/UI/GradientBox/GradientBox';
+import { Alert, IconButton, Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export const Authentication = () => {
-  const { data, mutate } = useMutation<
+  const { data, error, mutate } = useMutation<
     GetAuthResType,
     AxiosError,
     LoginDataType
   >('login', authApi.loginUser);
   const navigate = useNavigate();
+  const [ open, setOpen ] = useState(false);
 
   useEffect(() => {
     if (data) {
       localStorage.setItem('auth-token', data.jwtToken);
       navigate('/');
     }
-  }, [data, navigate]);
+    if (error)
+      setOpen(true)
+  }, [data, navigate, error]);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -33,6 +38,21 @@ export const Authentication = () => {
     });
   };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const action = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={handleClose}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
   return (
     <GradientBox
       sx={{
@@ -41,6 +61,23 @@ export const Authentication = () => {
         alignItems: 'center',
       }}
     >
+      <Snackbar
+        open={open}
+        anchorOrigin={{
+          vertical: 'bottom', horizontal: 'center'
+        }}
+      >
+        <Alert
+          severity="error"
+          sx={{
+            width: '100%' 
+          }}
+          variant="filled"
+          action={action}
+        >
+          Invalid Credentials.
+        </Alert>
+      </Snackbar>
       <Login login={onSubmit} />
     </GradientBox>
   );
