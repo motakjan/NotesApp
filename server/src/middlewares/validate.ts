cd40ac1cdd95e634f80a1804529d56d4c0168783
Yaroslav Pidlubnyi <ybiuza3@gmail.com>
2022-03-06 15:56:31 +0100

    Removed todo

--- server/src/middlewares/validate.ts
+++ server/src/middlewares/validate.ts
@@ -1,7 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 
 export const validate = (schema:any) => async (req:Request, res:Response, next:NextFunction) => {
-    // TODO validate query and params
     try {
         await schema.validate({
             body: req.body,
