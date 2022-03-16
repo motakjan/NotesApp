db07442f0b3e8c5f3cda7641142fedd5e4ecec0e
Jan Moták <61924776+motakjan@users.noreply.github.com>
2022-03-16 01:37:38 +0100

    login screen gif and framer component created

--- client/src/components/Authentication/LoginInfo/LoginInfo.tsx
+++ client/src/components/Authentication/LoginInfo/LoginInfo.tsx
@@ -1,36 +1,77 @@
 import { Box, Button } from '@mui/material';
-import { motion } from 'framer-motion';
 import { AnimatedNappLogo } from '../../UI/NappLogo/NappLogo';
+import { useState } from 'react';
+import { AnimatedBox } from '../../UI/FramerMotion/AnimatedDiv';
 
-const textVariants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { delay: 1.5, duration: 1 } },
-};
+export const LoginInfo = () => {
+  const [registerOpened, setRegisterOpened] = useState(false);
 
-const buttonVariants = {
-  hidden: { opacity: 0, x: -20 },
-  visible: { opacity: 1, x: 0, transition: { delay: 1.5, duration: 0.5 } },
-};
+  const registerVariants = {
+    hidden: { opacity: 0, x: -5000 },
+    visible: registerOpened ? { opacity: 1, x: 0, transition: { duration: 1 } } : {},
+  };
 
-export const LoginInfo = () => (
-  <Box sx={{ padding: '1.5rem', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-    <AnimatedNappLogo />
-    <motion.p className="loginText" initial="hidden" animate="visible" variants={textVariants}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel neque sint cumque minus exercitationem dicta
-      tempore error expedita, in amet culpa ipsum. Quis sint dolorem aperiam accusantium, ipsa cumque.
-    </motion.p>
-    <motion.div initial="hidden" animate="visible" variants={buttonVariants}>
-      <Button
+  const textVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { delay: 1.5, duration: 1 } },
+  };
+
+  const buttonVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0, transition: { delay: 1.5, duration: 0.5 } },
+  };
+
+  const registerButtonClicked = () => {
+    setRegisterOpened((prevState: boolean) => !prevState);
+  };
+
+  return (
+    <Box sx={{ padding: '1.5rem', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      <AnimatedNappLogo />
+      <AnimatedBox
         sx={{
-          color: 'black',
-          backgroundColor: 'white',
-          width: '12rem',
-          fontFamily: 'Nunito',
-          fontWeight: 700,
+          width: '25rem',
+          textAlign: 'center',
+          color: 'white',
+          fontSize: 'smaller',
+          margin: '15px',
+          fontFamily: 'Nunito, sans-serif',
         }}
+        initial="hidden"
+        animate="visible"
+        variants={textVariants}
       >
-        Register Now
-      </Button>
-    </motion.div>
-  </Box>
-);
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel neque sint cumque minus exercitationem
+        dicta tempore error expedita, in amet culpa ipsum. Quis sint dolorem aperiam accusantium, ipsa cumque.
+      </AnimatedBox>
+      <AnimatedBox initial="hidden" animate="visible" variants={buttonVariants}>
+        <Button
+          sx={{
+            color: 'black',
+            backgroundColor: 'white',
+            width: '12rem',
+            fontFamily: 'Nunito',
+            fontWeight: 700,
+          }}
+          onClick={() => registerButtonClicked()}
+        >
+          Register Now
+        </Button>
+      </AnimatedBox>
+      <AnimatedBox
+        initial="hidden"
+        animate="visible"
+        variants={registerVariants}
+        sx={{
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'white',
+          position: 'absolute',
+          zIndex: '50',
+          left: '0',
+          top: '0',
+        }}
+      />
+    </Box>
+  );
+};
--- client/src/pages/Authentication/Authentication.tsx
+++ client/src/pages/Authentication/Authentication.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { Login } from '../../components/Authentication/Login';
 import { Box } from '@mui/material';
 import { LoginInfo } from '../../components/Authentication/LoginInfo/LoginInfo';
+import './Authentication.css';
 
 export const Authentication = () => {
   const { data, mutate } = useMutation<GetAuthResType, AxiosError, LoginDataType>('login', authApi.loginUser);
@@ -35,7 +36,9 @@ export const Authentication = () => {
       sx={{
         display: 'flex',
         justifyContent: 'space-around',
-        backgroundImage: 'url(https://images.hdqwalls.com/download/dark-milky-way-galaxy-5k-5v-2560x1440.jpg)',
+        backgroundImage:
+          'url(https://media2.giphy.com/media/l3c64PElUdwpdbDPO/giphy.gif?cid=ecf05e47aze445q2nnkv7dtohlkb3rpo8v0sta8mfax8f4lr&rid=giphy.gif&ct=g)',
+        backgroundSize: 'cover',
         width: '100vw',
         height: '100vh',
       }}
